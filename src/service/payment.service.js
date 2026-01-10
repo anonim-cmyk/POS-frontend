b8ab8ce3b1980e28a60d03f8a2ad587199b51dd6
anonim-cmyk <ilhamsyahdan123@gmail.com>
2026-01-10 13:49:39 +0700

    updated

--- src/service/payment.service.js
+++ src/service/payment.service.js
@@ -1,33 +1,41 @@
 import { createOrderMidtrans } from "../api/payment.api";
 
-export const processPayment = async ({
+export const processPayment = ({
   orderCode,
   amount,
   customer,
   table,
   method,
-  onSuccess,
-  onError,
 }) => {
+  // CASH = langsung resolve
   if (method === "cash") {
-    onSuccess();
-    return;
+    return Promise.resolve();
   }
 
-  const res = await createOrderMidtrans({
-    order_id: orderCode,
-    gross_amount: amount,
-    customer_name: customer.name,
-    customer_phone: customer.phone,
-    tableNo: table.tableNo,
-    tableId: table.tableId,
-    method,
-  });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await createOrderMidtrans({
+        order_id: orderCode,
+        gross_amount: amount,
+        customer_name: customer.name,
+        customer_phone: customer.phone,
+        tableNo: table.tableNo,
+        tableId: table.tableId,
+        method,
+      });
 
-  window.snap.pay(res.data.token, {
-    onSuccess,
-    onPending: onError,
-    onError,
-    onclose: onError,
+      if (!window.snap) {
+        return reject(new Error("Midtrans Snap not loaded"));
+      }
+
+      window.snap.pay(res.data.token, {
+        onSuccess: () => resolve(),
+        onPending: () => reject(new Error("Payment pending")),
+        onError: (err) => reject(err),
+        onClose: () => reject(new Error("Payment cancelled")),
+      });
+    } catch (err) {
+      reject(err);
+    }
   });
 };
